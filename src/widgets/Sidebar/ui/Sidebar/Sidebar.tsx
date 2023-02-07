27257e3876530bbb0a204766f6356711dc4e4288
Roman <roman.shemelin31@gmail.com>
2023-02-07 10:32:23 +0300

    create sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Sidebar.module.scss";
+
+import { PropsWithChildren, useState } from "react";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export function Sidebar(props: PropsWithChildren<SidebarProps>) {
+  const { className } = props;
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
+  return (
+    <div
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+    >
+      <button onClick={onToggle}>toggle</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher className={cls.lang}/>
+      </div>
+    </div>
+  );
+}
