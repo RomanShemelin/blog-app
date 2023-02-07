ea97be087c0638b36bbfcc4d3f8190ac9553b03b
Roman <roman.shemelin31@gmail.com>
2023-02-07 10:34:23 +0300

    add internationalization i18n

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,6 +5,7 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function bulidPlugins({
   paths,
+  isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HtmlWebpackPlugin({
@@ -14,6 +15,9 @@ export function bulidPlugins({
     new MiniCssExtractPlugin({
       filename: "css/[name].[contenthash:8].css",
       chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
     }),
   ];
 }
---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,31 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./LangSwitcher.module.scss";
+
+import type { PropsWithChildren } from "react";
+import { useTranslation } from "react-i18next";
+import { Button } from "../Button/Button";
+import { themeButton } from "../Button/Button";
+
+interface LangSwitcherProps {
+  className?: string;
+}
+
+export function LangSwitcher({
+  className,
+}: PropsWithChildren<LangSwitcherProps>) {
+  const { t, i18n } = useTranslation();
+
+  const toggle = () => {
+    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+  };
+
+  return (
+    <Button
+      className={classNames(cls.LangSwitcher, {}, [className])}
+      theme={themeButton.CLEAR}
+      onClick={toggle}
+    >
+      {t("Язык")}
+    </Button>
+  );
+}
