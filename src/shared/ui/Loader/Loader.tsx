03f2fbf36007abc402440377ca829e719456f311
Roman <roman.shemelin31@gmail.com>
2023-02-09 12:38:53 +0300

    add not found page, add loader

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import './Loader.scss'
+
+import type { PropsWithChildren } from 'react'
+
+interface LoaderProps {
+  className?: string
+}
+
+export function Loader (props: PropsWithChildren<LoaderProps>) {
+  const { className } = props
+
+  return (
+      <div className={classNames('lds-ellipsis', {}, [className])}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+      </div>
+  )
+}
