7499e9d636af1caca77fb94ef2c8cc2f19edf3f3
Roman <roman.shemelin31@gmail.com>
2023-02-12 21:05:10 +0300

    add bundle analyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     "@typescript-eslint/strict-boolean-expressions": "warn",
     "@typescript-eslint/naming-convention": "warn",
     "i18next/no-literal-string": ["error", { markupOnly: true }],
-    "max-len": ["error", {ignoreComments: true}]
+    "max-len": ["error", {ignoreComments: true}, { "code": 100 }]
   },
   globals: {
     __IS_DEV__: true,
