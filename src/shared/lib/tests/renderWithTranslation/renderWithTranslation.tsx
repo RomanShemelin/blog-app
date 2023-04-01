3b51c725ecfd179b91b71f99f3b2be503fa7ca33
Roman <roman.shemelin31@gmail.com>
2023-04-01 16:45:06 +0300

    add netlify config, fix api config

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import { type ReactNode } from 'react'
 import { I18nextProvider } from 'react-i18next'
-import i18nForTest from 'shared/config/i18n/i18nForTest'
+import i18nForTest from 'shared/config/i18n/i18nforTest'
 
 export function renderWithTranslation (component: ReactNode) {
   return render(
