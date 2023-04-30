fc5fb679929ce8f0146b49f41d9acf6b88fe961f
Roman <roman.shemelin31@gmail.com>
2023-04-30 16:07:10 +0300

    add star rating, add Rating entity and rating card

---
+++ src/entities/Rating/ui/RatingCard/RatingCard.tsx
@@ -0,0 +1,88 @@
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './RatingCard.module.scss';
+
+import { useTranslation } from 'react-i18next';
+import { Card } from '@/shared/ui/Card/Card';
+import { HStack, VStack } from '@/shared/ui/Stack';
+import { Text } from '@/shared/ui/Text/Text';
+import { StarRating } from '@/shared/ui/StarRating/StarRating';
+import { useCallback, useState } from 'react';
+import { Modal } from '@/shared/ui/Modal/Modal';
+import { Input } from '@/shared/ui/Input/Input';
+import { Button, ButtonTheme } from '@/shared/ui/Button/Button';
+import { BrowserView, MobileView } from 'react-device-detect';
+import { Drawer } from '@/shared/ui/Drawer/Drawer';
+
+interface RatingCardProps {
+  className?: string
+  title?: string
+  feedbackTitle?: string
+  hasfeedback?: boolean
+  onCancel?: (starCount: number) => void
+  onAccept?: (starcount: number, feedback?: string) => string
+}
+
+export function RatingCard (props: RatingCardProps) {
+  const { className, feedbackTitle, hasfeedback, onAccept, title } =
+    props;
+  const { t } = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [starsCount, setStarsCount] = useState(0);
+  const [feedback, setFeedback] = useState('');
+
+  const onSelectStars = useCallback(
+    (selectedStarsCount: number) => {
+      setStarsCount(selectedStarsCount);
+      if (hasfeedback) {
+        setIsModalOpen(true);
+      } else {
+        onAccept?.(selectedStarsCount);
+      }
+      setIsModalOpen(true);
+    },
+    [hasfeedback, onAccept]
+  );
+
+  const acceptHandle = useCallback(() => {
+    setIsModalOpen(false);
+    onAccept?.(starsCount, feedback);
+  }, [feedback, onAccept, starsCount]);
+
+  const cancelHandle = useCallback(() => {
+    setIsModalOpen(false);
+    onAccept?.(starsCount);
+  }, [onAccept, starsCount]);
+
+  const modalContent = (
+    <VStack max gap='32'>
+      <Text title={feedbackTitle} />
+      <Input value={feedback} onChange={setFeedback} placeholder={t('your feedback')} />
+      <HStack max gap='16' justify='end'>
+        <Button onClick={cancelHandle} theme={ButtonTheme.OUTlINE_RED}>
+          {t('Close')}
+        </Button>
+        <Button onClick={acceptHandle}>{t('Send')}</Button>
+      </HStack>
+    </VStack>
+  );
+
+  return (
+    <Card className={classNames(cls.RatingCard, {}, [className])}>
+      <VStack align='center' gap='8'>
+        <Text title={title} />
+        <StarRating size={40} onSelect={onSelectStars} />
+      </VStack>
+      <Modal isOpen={isModalOpen}></Modal>
+      <BrowserView>
+        <Modal isOpen={isModalOpen} lazy>
+          {modalContent}
+        </Modal>
+      </BrowserView>
+      <MobileView>
+        <Drawer isOpen={isModalOpen} lazy onClose={cancelHandle}>
+          {modalContent}
+        </Drawer>
+      </MobileView>
+    </Card>
+  );
+}
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import { Page } from '@/widgets/Page/Page'
+import { StarRating } from '@/shared/ui/StarRating/StarRating'
+import { RatingCard } from '@/entities/Rating'
 
 const MainPage = () => {
   const { t } = useTranslation()
@@ -7,6 +9,8 @@ const MainPage = () => {
   return (
     <Page>
       {t('Main page')}
+      <StarRating/>
+      <RatingCard title="Оставить отзыв" feedbackTitle="Оставьте отзыв о статье"/>
     </Page>
   )
 }
